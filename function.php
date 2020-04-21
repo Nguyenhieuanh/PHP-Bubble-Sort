d8fe8e290c4406ae52be024636e4d5bb85d40ac6
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-04-21 08:40:28 +0700

    secondcommit

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+function bubbleSort($arr)
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($arr[$i] > $arr[$j]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$i];
+                $arr[$i] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+
+function show($arr)
+{
+    foreach ($arr as $item) {
+        echo "$item, ";
+    }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include_once "function.php";
+
+$arr = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+
+show(bubbleSort($arr));
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>[Bài đọc] Sắp xếp nổi bọt</title>
+</head>
+<body>
+
+</body>
+</html>
